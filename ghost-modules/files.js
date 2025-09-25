2f292db667c7b8b9402631519ecca8bbf13e88d9
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-09-25 10:29:35 -0300

    gh

--- ghost-modules/files.js
+++ ghost-modules/files.js
@@ -1,14 +1,30 @@
 // files.js
-export function download(URL, filename = null) {
-  const link = document.createElement("a");
-  link.href = URL;
+export function download(data, filename = "download.txt") {
+  let url;
 
-  if (!filename) {
-    filename = URL.split("/").pop() || "download";
+  if (typeof data === "string" && (data.startsWith("http://") || data.startsWith("https://"))) {
+    url = data;
+  } else if (typeof data === "string") {
+    const blob = new Blob([data], { type: "text/plain" });
+    url = URL.createObjectURL(blob);
+  } else if (data instanceof Blob) {
+    url = URL.createObjectURL(data);
+  } else if (typeof data === "object") {
+    const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+    url = URL.createObjectURL(blob);
+  } else {
+    throw new Error("Unsupported data type for download()");
   }
+
+  const link = document.createElement("a");
+  link.href = url;
   link.download = filename;
 
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+
+  if (!data.startsWith?.("http")) {
+    URL.revokeObjectURL(url);
+  }
 }
