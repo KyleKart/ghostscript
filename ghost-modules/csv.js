49b93ed58bc06d185d0d16471e9cf03ebe2401fe
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-09-24 21:55:46 -0300

    f

---
+++ ghost-modules/csv.js
@@ -0,0 +1,12 @@
+export function parse(csvText, delimiter = ",") {
+  return csvText
+    .trim()
+    .split("\n")
+    .map(row => row.split(delimiter).map(cell => cell.trim()));
+}
+
+export function stringify(data, delimiter = ",") {
+  return data
+    .map(row => row.map(cell => `"${String(cell).replace(/"/g, '""')}"`).join(delimiter))
+    .join("\n");
+}
