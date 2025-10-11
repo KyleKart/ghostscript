1ef8e787d2fef937b18c3b038835077173ae43fc
KyleKart <71104484+KyleKart@users.noreply.github.com>
2025-10-11 18:58:55 -0300

    m

--- ghost-modules/warp.js
+++ ghost-modules/warp.js
@@ -1,7 +1,43 @@
-// warp.js
+let _info = {};
+let _blocks = {};
+let _menus = {};
 
-export function info(array) {
+export function info(object) {
+    _info = object;
 }
 
-export function block(type, text, opcode, values) {
+export function block(type, text, opcode, values = []) {
+    _blocks[opcode] = { type, text, values };
+}
+
+export function menus(object) {
+    _menus = object;
+}
+
+// Export the Extension class
+export class Extension {
+    getInfo() {
+        return {
+            ..._info,
+            blocks: Object.values(_blocks).map(b => ({
+                opcode: b.opcode,
+                blockType: Scratch.BlockType[b.type.toUpperCase()],
+                text: b.text,
+                arguments: b.values.reduce((acc, v) => {
+                    acc[v.name] = v;
+                    return acc;
+                }, {}),
+                extensions: ["colours_looks"]
+            })),
+            menus: _menus
+        };
+    }
+}
+
+export function func(opcode, func) {
+    Extension.prototype[opcode] = func;
+}
+
+export function register(ext) {
+    Scratch.extensions.register(ext);
 }
